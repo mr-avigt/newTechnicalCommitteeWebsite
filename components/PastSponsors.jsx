ccd66c1a9eb7be5a94590a277cadd4629927317b
Krishna41357 <154420450+Krishna41357@users.noreply.github.com>
2024-12-31 13:47:03 +0530

    Update PastSponsors.jsx

--- components/PastSponsors.jsx
+++ components/PastSponsors.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 
 const SponsorCard = ({image}) => (
-   <div className="w-40 h-40 bg-transparent border-4 border-[#F4CF8B] rotate-45 m-16 overflow-hidden flex flex-col flex-end transform transition-all duration-300 hover:scale-105 ">
-     <div className="absolute inset-0  bg-cover bg-no-repeat bg-center rotate-[-45deg] "style={{ filter: 'grayscale(100%)' , backgroundImage:`url(${image})`,backgroundSize: 'cover',
+   <div className="w-40 h-40 bg-transparent border-4 border-[#F4CF8B] rotate-45 m-16 overflow-hidden flex flex-col flex-end transform transition-all duration-300 hover:scale-105 hover:shadow-[0_4px_12px_rgba(255,215,0,0.7),0_0_20px_rgba(0,0,0,0.5)] ">
+     <div className="absolute inset-0  bg-cover bg-no-repeat bg-center  "style={{ filter: 'grayscale(100%)' , backgroundImage:`url(${image})`,backgroundSize: 'cover',scale:"2.2",
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'center',
       transform: 'rotate(-45deg)',
        }} ></div>
-     
+     <img
+       src="/purple.png"
+       alt=""
+       className="relative bottom-0 right-0 left-14 top-20 rotate-45"
+       style={{ scale: '0.6',filter: 'grayscale(100%)' }}
+     />
    </div>
  );
  

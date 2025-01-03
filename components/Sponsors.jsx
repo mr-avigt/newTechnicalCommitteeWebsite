de97649b40898627a33638160f7a06312e7d1381
Krishna41357 <154420450+Krishna41357@users.noreply.github.com>
2025-01-03 23:04:12 +0530

    Update Sponsors.jsx

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -41,16 +41,21 @@ const Sponsors = () => {
           </div>
         </div>
 
+        
         <div className="w-40 h-40 bg-transparent border-4 border-[#F4CF8B] rotate-45 m-16 overflow-hidden flex flex-col flex-end transform transition-all duration-300 hover:scale-105 hover:shadow-[0_4px_12px_rgba(255,215,0,0.7),0_0_20px_rgba(0,0,0,0.5)] text-center relative group">
           {" "}
-          <div className="absolute inset-0 bg-[url('/TC_Logo.webp')]  bg-cover bg-no-repeat bg-center rotate-[-45deg]"></div>
+          <div className="absolute inset-0  bg-cover bg-no-repeat bg-center  "style={{   backgroundImage:`url("/sponsor1.png")`,backgroundSize: 'cover',scale:"2.2",
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      transform: 'rotate(-45deg)',
+       }} ></div>
           <div
             id="2"
             className="absolute inset-0 flex items-center justify-center opacity-0 pointer-events-none group-hover:opacity-100 transition-opacity duration-300"
             style={{ rotate: "-45deg" }}
           >
             <span className="text-xl font-bold tracking-wide text-[#f4cf8b] ">
-              S P O N S O R<br />
+              {/* S P O N S O R<br /> */}
             </span>
           </div>
         </div>

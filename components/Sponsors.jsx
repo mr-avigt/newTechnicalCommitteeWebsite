a20b519f9bd36c8cc8e85f5d522813e15b4e2ef8
Kartikay Verma <122208593+kartikayverma56@users.noreply.github.com>
2025-01-03 17:25:44 +0530

    Update Sponsors.jsx

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -27,7 +27,7 @@ const Sponsors = () => {
             className="absolute inset-0 flex items-center justify-center opacity-0 pointer-events-none group-hover:opacity-100 transition-opacity duration-300"
           >
           <span className="text-2xl font-bold tracking-wide text-[#f4cf8b] flex flex-col flex-wrap sm:whitespace-normal">
-  Grownli <br className="sm:hidden" /> AI
+  Growli <br className="sm:hidden" /> AI
   <span className="text-base">(OUR TITLE SPONSOR)</span>
 </span>
 

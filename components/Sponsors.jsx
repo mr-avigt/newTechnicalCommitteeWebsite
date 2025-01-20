120f6e22876fdfd03a9798ef52d3a3ff3a0df0ee
Kartikay Verma <122208593+kartikayverma56@users.noreply.github.com>
2025-01-20 23:15:35 +0530

    Update Sponsors.jsx

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -110,7 +110,7 @@ const Sponsors = () => {
                   ></div>
                 </a>
               </div>
-            </a>
+           
           </div>
 
           <div className="w-40 h-40 bg-transparent border-4 border-[#F4CF8B] rotate-45 m-16 overflow-hidden flex flex-col flex-end transform transition-all duration-300 hover:scale-105 hover:shadow-[0_4px_12px_rgba(255,215,0,0.7),0_0_20px_rgba(0,0,0,0.5)] text-center relative group">
